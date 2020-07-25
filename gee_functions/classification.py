37e8c08118d121cf843095555d7097c411cacd43
Javier Martinez <javi.martinez.lopez@gmail.com>
2020-07-25 17:06:31 +0200

    bug fixed

--- gee_functions/classification.py
+++ gee_functions/classification.py
@@ -289,14 +289,13 @@ def create_training_areas(aoi, data_loc, aoi_name, year_string, clf_folder=None,
                 data_image.select('NDWBI_mean').gt(-.45)).And(
                 data_image.select('NDWBI_min').gt(-.5))
 
-            mask_irrigated_trees = 	#data_image.select('slope').lte(5).And(
+            mask_irrigated_trees = data_image.select('GCVI_min').gte(0.98)).And(	#data_image.select('slope').lte(5).And(
                 #data_image.select('WGI_max').gte(0.25)).And(
                 #data_image.select('WGI_mean').gte(0.1)).And(
                 #data_image.select('NDBI_min').lte(-0.1)).And(
                 #data_image.select('NDWI_min').gte(0)).And(
                 #data_image.select('GCVI_max').gte(1.5)).And(
                 #data_image.select('GCVI_mean').gte(1.25)).And(
-                data_image.select('GCVI_min').gte(0.98)).And(
                 #data_image.select('MTI').gte(3)).And(
                 #data_image.select('NDBI_max').lte(0)).And(
                 #data_image.select('NDBI_mean').lte(0)).And(
